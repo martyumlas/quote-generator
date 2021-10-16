f129242735892e59535aa73e4c6330c3a2e21bad
martyumlas@gmail.com <martyumlas@gmail.com>
2021-10-16 18:29:05 +0800

    add a loader

--- script.js
+++ script.js
@@ -3,23 +3,39 @@ const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
 const twitterBtn = document.getElementById('twitter')
 const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
 
 let apiQuotes = []
 
-//show new Quote
+//show loading
+function loading() {
+    loader.hidden = false
+    quoteContainer.hidden = true
+}
 
+//hide loading
+
+function complete() {
+    quoteContainer.hidden = false
+    loader.hidden = true
+}
+
+//show new Quote
 function newQuote() {
+    loading()
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
     //check if author is null
     authorText.textContent = quote.author ? quote.author: 'unknown'
     //check quote if long
     quote.text.length > 50 ? quoteText.classList.add('long-quote') : quoteText.classList.remove('long-quote')
     quoteText.textContent = quote.text
+    complete()
     
 }
 
 // get quotes from api
 async function getQuotes () {
+    loading()
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl)
