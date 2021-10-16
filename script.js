cb17572febb5f007268c8b913a7f05b5e4b1ebc8
martyumlas@gmail.com <martyumlas@gmail.com>
2021-10-16 18:06:46 +0800

    dom manipulation

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
 let apiQuotes = []
 
 //show new Quote
 
 function newQuote() {
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-
-    console.log(quote)
+    //check if author is null
+    authorText.textContent = quote.author ? quote.author: 'unknown'
+    //check quote if long
+    quote.text.length > 50 ? quoteText.classList.add('long-quote') : quoteText.classList.remove('long-quote')
+    quoteText.textContent = quote.text
+    
 }
 
 // get quotes from api
@@ -14,11 +24,26 @@ async function getQuotes () {
     try {
         const response = await fetch(apiUrl)
         apiQuotes = await response.json()
+     
         newQuote()
     } catch (error) {
-        
+        console.log(error)
     }
 }
+
+
+//tweet quote
+
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`
+    window.open(twitterUrl, '_blank')
+}
+
+//event listeners
+
+newQuoteBtn.addEventListener('click', newQuote)
+twitterBtn.addEventListener('click', tweetQuote)
+
 
 // on load
 getQuotes()
